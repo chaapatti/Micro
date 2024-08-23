88d6429d83d1e1d1a6ea5fc3a6f7fce6deaaffa0
chaapatti <137924830+chaapatti@users.noreply.github.com>
2024-08-23 17:41:10 +0000

    multiplication

--- main.c
+++ main.c
@@ -3,7 +3,8 @@
 typedef struct Value {
     float data;
     float grad;
-    struct Value **prev;
+    struct Value **prev; // this stores the address of the array.
+    char operator;
 // void pointers cannot be dereferenced
 
 }Value;
@@ -13,13 +14,33 @@ Value *add(Value *a, Value *b) {
     printf("a: %p, b: %p", a, b);
     newValue->data = a->data + b->data;
     newValue->grad = 0.0;
-    Value *children[2] = {a, b};
-    
+    Value **children = (Value**)malloc(sizeof(Value*)*2);
+    children[0] = a; // children is a array of valueStruct pointers. That is it is an array containing pointers to the value struct.
+    children[1] = b;
+    newValue->operator = '+';
     newValue->prev = children;
-    printf("\nvalue inside function: %p, |  %p\n", newValue->prev, children);
+    printf("\nvalue inside function: %p, |  %p\n", newValue->prev, &children);
     printf("\nvalue inside function: %p, |  %p\n", children[0], children[1]);
     return newValue;
 
+
+}
+
+Value *mul(Value*self, Value*other) {
+
+    Value*newMultiply = (Value*)malloc(sizeof(Value));
+    newMultiply->data = self->data * other->data;
+    Value **children = (Value**)malloc(sizeof(Value*)*2);
+    children[0] = self;
+    children[1] = other;
+    newMultiply->operator = '*';
+    newMultiply->prev = children;
+    return newMultiply;
+
+
+}
+
+Value* power(Value* self, Value * other) {
 
 }
 
@@ -31,8 +52,10 @@ int main() {
     Value *newValue = add(&a, &b);
     printf("%p, |  %p", &a, &b);
     printf("\ndata: %f", newValue->data);
-    printf("\n%p, |  %p\n", newValue->prev, newValue->prev[1]);
-
+    printf("\n%f\n", newValue->prev[1]->data);
+    free(newValue->prev);
+    free(newValue);
     return 0;
 }
+
 
